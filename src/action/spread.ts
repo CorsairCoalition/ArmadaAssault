a03dc35137f3f3c9aee81c76aca5a2f7073c2d91
Aaditya Bhatia <aadityabhatia@gmail.com>
2023-04-09 01:34:06 -0400

    created action: Spread

---
+++ src/action/spread.ts
@@ -0,0 +1,66 @@
+/// <reference path="../app.d.ts" />
+
+import Action from './action.js'
+import { Log, later, random } from '../utils.js'
+import { GameMap } from '../gameMap.js'
+
+export default class Spread extends Action {
+
+	nextMove: number = 12
+	lastRecommendation: RedisData.Recommendation
+
+	async generateRecommendation(gameState: Record<string, any>, gameMap: GameMap): Promise<RedisData.Recommendation> {
+		let turn = gameState[RedisData.KEY.TURN]
+		if (turn < this.nextMove) {
+			return
+		}
+
+		const moveableTiles = gameMap.getMoveableTiles()
+		const actions: GeneralsIO.Attack[] = []
+
+		for (const start of moveableTiles) {
+			const adjacentTiles = gameMap.getAdjacentTiles(start)
+			let bestTargetTile: GeneralsIO.Tile | null = null
+			let maxRemainingArmies = 0
+
+			for (const direction in adjacentTiles) {
+				if (adjacentTiles.hasOwnProperty(direction)) {
+					const targetTile = adjacentTiles[direction]
+
+					if (gameMap.isWalkable(targetTile)) {
+						const remainingArmies = gameMap.remainingArmiesAfterAttack(start, targetTile.index)
+
+						if ((gameMap.isAdjacentToFog(targetTile.index) || remainingArmies > 0) && remainingArmies > maxRemainingArmies) {
+							bestTargetTile = targetTile
+							maxRemainingArmies = remainingArmies
+						}
+					}
+				}
+			}
+
+			if (bestTargetTile) {
+				actions.push({
+					start: start,
+					end: bestTargetTile.index
+				})
+			}
+		}
+
+		if (actions.length === 0) {
+			this.nextMove = turn + 5
+			return
+		}
+
+		this.nextMove = turn + actions.length
+
+		this.lastRecommendation = {
+			interrupt: false,
+			actions: actions,
+			recommender: this.constructor.name,
+			confidence: 0,
+			priority: 0,
+		}
+
+		return this.lastRecommendation
+	}
+}
